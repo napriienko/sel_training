4c5f143429bb7541128c9fa9eaac75586d020416
napriienko <napriyenko@gmail.com>
2016-11-26 11:42:17 +0100

    added test for homework8
    Checks that each product on the litecart main page has exactly one
    sticker

---
+++ sel-training-homework/sel-training-homework/homework8.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace sel_training_homework
+{
+    [TestFixture]
+    public class homework8
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
+        }
+
+        [Test]
+        public void CheckProductStickers()
+        {
+            driver.Url = "http://localhost/litecart";
+
+            //get all products
+            IList<IWebElement> products = driver.FindElements(By.CssSelector("li.product.column.shadow.hover-light"));
+            foreach (IWebElement i in products)
+            {
+                //check each product has one sticker element
+                Assert.IsTrue(i.FindElements(By.CssSelector(".sticker")).Count == 1);
+            }
+ 
+
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
