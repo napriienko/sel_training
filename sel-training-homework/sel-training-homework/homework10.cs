69faa1dd90a6275cb8d671adb0218fec13a363ed
napriienko <napriyenko@gmail.com>
2016-11-27 18:58:09 +0100

    added homework 9 and 10

---
+++ sel-training-homework/sel-training-homework/homework10.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace sel_training_homework
+{
+    [TestFixture]
+    public class homework10
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
+        }
+
+        [Test]
+        public void CheckCorrectProductPageOpened()
+        {
+            driver.Url = "http://localhost/litecart";            
+            //get all campaign products elements main page
+            IList<IWebElement> products = driver.FindElements(By.CssSelector("#box-campaigns ul li"));
+            //get name of the first campaign product
+            string name = products[0].FindElement(By.CssSelector(".name")).Text;
+            //get element of the regular price and campaing price
+            IWebElement regularPrice = products[0].FindElement(By.CssSelector(".regular-price"));
+            IWebElement campaignPrice = products[0].FindElement(By.CssSelector(".campaign-price"));
+            //get text of regular and campaing price
+            string regularPriceValue = regularPrice.Text;
+            string campaingPriceValue = campaignPrice.Text;
+            //get classes used for prices
+            string regularPriceValueClass = regularPrice.GetAttribute("className");
+            string campaingPriceValueClass = campaignPrice.GetAttribute("className");
+            //get font, color, text decoration of the regular price
+            string regularPriceColor = regularPrice.GetCssValue("color");
+            string regularPriceFontSize = regularPrice.GetCssValue("font-size");
+            string regularPriceText = regularPrice.GetCssValue("text-decoration");
+            //get font, color, text decoration of the campaign price
+            string campaingPriceColor = campaignPrice.GetCssValue("color");
+            string campaingPriceFontSize = campaignPrice.GetCssValue("font-size");
+            string campaingPriceText = campaignPrice.GetCssValue("text-decoration");
+            //navigate to the product page from the main page
+            products[0].Click();
+            //get product element
+            IWebElement productPage = driver.FindElement(By.CssSelector("#box-product"));
+            //get product name
+            string nameProductPage = productPage.FindElement(By.CssSelector(".title")).Text;
+            //get product prices elements
+            IWebElement regularPriceProductPage = productPage.FindElement(By.CssSelector(".regular-price"));
+            IWebElement campaignPriceProductPage = productPage.FindElement(By.CssSelector(".campaign-price"));
+            //get texts for regular and campaing price 
+            string regularPriceValueProductPage = regularPriceProductPage.Text;
+            string campaingPriceValueProductPage = campaignPriceProductPage.Text;
+            //get classes attributes
+            string regularPriceValuepPoductPageClass = regularPriceProductPage.GetAttribute("className");
+            string campaingPriceValueProductPageClass = campaignPriceProductPage.GetAttribute("className");
+            //get regular price styles
+            string regularPriceColorProductPage = regularPriceProductPage.GetCssValue("color");
+            string regularPriceFontSizeProductPage = regularPriceProductPage.GetCssValue("font-size");
+            string regularPriceTextProductPage = regularPriceProductPage.GetCssValue("text-decoration");
+            //get campaing price styles
+            string campaingPriceColorProductPage = campaignPriceProductPage.GetCssValue("color");
+            string campaingPriceFontSizeProductPage = campaignPriceProductPage.GetCssValue("font-size");
+            string campaingPriceTextProductPage = campaignPriceProductPage.GetCssValue("text-decoration");
+
+            //check nproduct name on the main page and on the product page are the same
+            Assert.AreEqual(name, nameProductPage);
+            //check regular prices are the same
+            Assert.AreEqual(regularPriceValue, regularPriceValueProductPage);
+            //check campaign prices are the same
+            Assert.AreEqual(campaingPriceValue, campaingPriceValueProductPage);
+            //check classes used for styling are the same
+            Assert.AreEqual(regularPriceValueClass, regularPriceValuepPoductPageClass);
+            Assert.AreEqual(campaingPriceValueClass, campaingPriceValueProductPageClass);
+            //check text decoration are the same
+            Assert.AreEqual(regularPriceText, regularPriceTextProductPage);
+            Assert.AreEqual(campaingPriceText, campaingPriceTextProductPage);
+            //Note: can not compare color and font sizes of the prices as they are different so used to compare classes
+
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
---
+++ sel-training-homework/sel-training-homework/homework9.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace sel_training_homework
+{
+    [TestFixture]
+    public class homework9
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
+        }
+
+        [Test]
+        public void CheckCountriesAndZonesAreSorted()
+        {
+            driver.Url = "http://localhost/litecart/admin/?app=countries&doc=countries";
+            driver.FindElement(By.Name("username")).SendKeys("admin");
+            driver.FindElement(By.Name("password")).SendKeys("admin");
+            driver.FindElement(By.Name("login")).Click();
+            wait.Until(ExpectedConditions.ElementIsVisible(By.Id("sidebar")));
+
+            //get all coutries rows elements
+            IList<IWebElement> countries = driver.FindElements(By.CssSelector(".dataTable .row"));
+            //get total namber of countries rows
+            int countriesNumber = driver.FindElements(By.CssSelector(".dataTable .row")).Count;           
+
+            for (int i=0; i < countriesNumber-1; i++)
+            {
+                //compare contry name with the next country name
+                string countryName = countries[i].FindElement(By.CssSelector("a")).Text;
+                string nextCountryName = countries[i+1].FindElement(By.CssSelector("a")).Text;
+                int compare = String.Compare(countryName, nextCountryName);
+                //if result <0 then two strings are sorted in acsending order
+                Assert.IsTrue(compare < 0);
+                //get zones
+                IWebElement zoneElement = countries[i].FindElement(By.CssSelector("td:nth-child(6)"));
+                string zonesNumber = zoneElement.Text;
+                //if it has more than o zone the navigate to zones page
+                if (Convert.ToInt32(zonesNumber) > 0)
+                {
+                    countries[i].FindElement(By.CssSelector("a")).Click();
+                    //get all zones rows
+                    IList<IWebElement> zonesElements = driver.FindElements(By.CssSelector("#table-zones tr"));
+                    //check they are sorted
+                    for (int j=1; j<zonesElements.Count-2; j++)
+                    {
+                        string zone = zonesElements[j].FindElement(By.CssSelector("td:nth-child(3)")).GetAttribute("textContent");
+                        string zoneNext = zonesElements[j+1].FindElement(By.CssSelector("td:nth-child(3)")).GetAttribute("textContent");
+                        Assert.IsTrue(String.Compare(zone, zoneNext) < 0);
+                    }
+                    //navigate back to the Countries list
+                    driver.FindElement(By.CssSelector("#box-apps-menu li:nth-child(3)")).Click();
+                }
+                //get again all coutries rows elements as the page is reloaded
+                countries = driver.FindElements(By.CssSelector(".dataTable .row"));
+            }
+ 
+            
+        }
+
+        [Test]
+        public void CheckGeoZonesAreSorted()
+        {
+            driver.Url = "http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones";
+            driver.FindElement(By.Name("username")).SendKeys("admin");
+            driver.FindElement(By.Name("password")).SendKeys("admin");
+            driver.FindElement(By.Name("login")).Click();
+            wait.Until(ExpectedConditions.ElementIsVisible(By.Id("sidebar")));
+
+            //getting all geo zones
+            IList<IWebElement> zonesElement = driver.FindElements(By.CssSelector(".dataTable .row"));
+            //for each geo zone navigate to the zones page
+            for (int i=0; i<zonesElement.Count; i++)
+            {
+                zonesElement[i].FindElement(By.CssSelector("a")).Click();
+                IList<IWebElement> zones = driver.FindElements(By.CssSelector("select[name*='zone_code'] option[selected='selected']"));
+                //check zones are sorted
+                for (int j = 1; j < zones.Count - 1; j++)
+                {
+                    string zone = zones[j].Text;
+                    string zoneNext = zones[j + 1].Text;
+                    Assert.IsTrue(String.Compare(zone, zoneNext) < 0);
+                }
+                //navigate back to geo zones page, is done by Cancel button
+                driver.FindElement(By.Name("cancel")).Click();
+                //getting list of geo zones again
+                zonesElement = driver.FindElements(By.CssSelector(".dataTable .row"));
+            }
+
+        }
+
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
